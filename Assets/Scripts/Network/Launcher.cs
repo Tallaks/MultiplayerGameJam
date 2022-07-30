dd417ab869694b0808f31472e898deb537e140f9
Eric.Lighthall <ericlighthall2@gmail.com>
2022-07-29 21:57:47 -0700

    Lobby updates list of players on both clients
    When players leave a room, that change is now reflected in the lobby display panel.

--- Assets/Scripts/Network/Launcher.cs
+++ Assets/Scripts/Network/Launcher.cs
@@ -106,6 +106,18 @@ public class Launcher : MonoBehaviourPunCallbacks {
         }
     }
 
+    public override void OnPlayerEnteredRoom(Player newPlayer) {
+        TMP_Text newPlayerLabel = Instantiate(playerNameLabel, playerNameLabel.transform.parent);
+        newPlayerLabel.text = newPlayer.NickName;
+        newPlayerLabel.gameObject.SetActive(true);
+
+        allPlayerNames.Add(newPlayerLabel);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer) {
+        ListAllPlayers();
+    }
+
     public override void OnCreateRoomFailed(short returnCode, string message) {
         errorText.text = "Failed To Create Room: " + message;
         CloseMenus();
