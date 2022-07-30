635bdf94d2984229a5004f350fd18d799b729e00
Eric.Lighthall <ericlighthall2@gmail.com>
2022-07-30 03:07:29 -0700

    Fix all characters being controlled
    Fixed an issue where one player could control all players on the server.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -31,52 +31,58 @@ public class PlayerController : MonoBehaviourPunCallbacks
     }
 
     private void Update() {
-        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+        // Only control your your character
+        if (photonView.IsMine) {
 
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+            mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
 
-        verticalRotStore += mouseInput.y;
-        verticalRotStore = Mathf.Clamp(verticalRotStore, -80f, 80f);
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
 
-        if(invertLook) {
-            viewPoint.rotation = Quaternion.Euler(verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
-        }
-        else {
-            viewPoint.rotation = Quaternion.Euler(-verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
-        }
+            verticalRotStore += mouseInput.y;
+            verticalRotStore = Mathf.Clamp(verticalRotStore, -80f, 80f);
 
-        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+            if (invertLook) {
+                viewPoint.rotation = Quaternion.Euler(verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
+            }
+            else {
+                viewPoint.rotation = Quaternion.Euler(-verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
+            }
 
-        float yVel = movement.y;
-        movement = ((transform.forward * moveDir.z) + (transform.right * moveDir.x)).normalized;
-        movement.y = yVel;
+            moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
 
-        if(controller.isGrounded) {
-            movement.y = 0f;
-        }
+            float yVel = movement.y;
+            movement = ((transform.forward * moveDir.z) + (transform.right * moveDir.x)).normalized;
+            movement.y = yVel;
 
-        isGrounded = Physics.Raycast(groundCheck.position, Vector3.down, .25f, groundLayers);
+            if (controller.isGrounded) {
+                movement.y = 0f;
+            }
 
-        if(Input.GetButtonDown("Jump") && isGrounded) {
-            movement.y = jumpForce;
-        }
+            isGrounded = Physics.Raycast(groundCheck.position, Vector3.down, .25f, groundLayers);
 
-        movement.y += Physics.gravity.y * Time.deltaTime * gravityMod;
+            if (Input.GetButtonDown("Jump") && isGrounded) {
+                movement.y = jumpForce;
+            }
 
-        controller.Move(movement * moveSpeed * Time.deltaTime);
+            movement.y += Physics.gravity.y * Time.deltaTime * gravityMod;
 
-        if (Input.GetKeyDown(KeyCode.Escape)) {
-            Cursor.lockState = CursorLockMode.None;
-        }
-        else if (Cursor.lockState == CursorLockMode.None) {
-            if(Input.GetMouseButtonDown(0)) {
-                Cursor.lockState = CursorLockMode.Locked;
+            controller.Move(movement * moveSpeed * Time.deltaTime);
+
+            if (Input.GetKeyDown(KeyCode.Escape)) {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else if (Cursor.lockState == CursorLockMode.None) {
+                if (Input.GetMouseButtonDown(0)) {
+                    Cursor.lockState = CursorLockMode.Locked;
+                }
             }
         }
     }
 
     private void LateUpdate() {
-        cam.transform.position = viewPoint.position;
-        cam.transform.rotation = viewPoint.rotation;
+        if (photonView.IsMine) {
+            cam.transform.position = viewPoint.position;
+            cam.transform.rotation = viewPoint.rotation;
+        }
     }
 }
